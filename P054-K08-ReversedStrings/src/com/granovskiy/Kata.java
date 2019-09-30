dbb7d153beece8b478cde7634793d9e994160afb
Sviatoslav <s.granovskiy@gmail.com>
2019-09-30 20:05:12 +0300

    Initial commit

---
+++ P054-K08-ReversedStrings/src/com/granovskiy/Kata.java
@@ -0,0 +1,7 @@
+package com.granovskiy;
+
+public class Kata {
+    public static String solution(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+}
---
+++ P055-K08-ConvertaBooleantoaString/src/com/granovskiy/Kata.java
@@ -0,0 +1,16 @@
+package com.granovskiy;
+
+public class Kata {
+    public static String convert(boolean b){
+        if(b) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
