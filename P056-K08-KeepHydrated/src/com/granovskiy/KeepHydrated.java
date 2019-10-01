6fcef9d9a5b07b5885a62cbb4de8458a15f9eee1
Sviatoslav <s.granovskiy@gmail.com>
2019-10-01 08:38:08 +0300

    Initial commit

---
+++ P056-K08-KeepHydrated/src/com/granovskiy/KeepHydrated.java
@@ -0,0 +1,8 @@
+package com.granovskiy;
+
+public class KeepHydrated  {
+    public int Liters(double time)  {
+        return (int) Math.floor(time / 2);
+        //  return (int) (time * 0.5);
+    }
+}
