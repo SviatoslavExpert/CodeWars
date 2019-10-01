9d1bb1221c6067df7cc1b5ebb9a0f519d9e2be7c
Sviatoslav <s.granovskiy@gmail.com>
2019-10-01 16:42:47 +0300

    Initial commit

---
+++ P062-K08-TheFeastofManyBeasts/src/com/granovskiy/Kata.java
@@ -0,0 +1,14 @@
+package com.granovskiy;
+
+public class Kata {
+    public static boolean feast(String beast, String dish) {
+        return beast.charAt(0) == dish.charAt(0)
+                && beast.charAt(beast.length() - 1)
+                == dish.charAt(dish.length() - 1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(feast("heron", "hanan"));
+    }
+}
