42cf2572fddf44e0d9631d633e0ec4a8d95f7598
Sviatoslav <s.granovskiy@gmail.com>
2019-10-01 09:25:16 +0300

    Initial commit

---
+++ P057-K08-AllStarCodeChallenge#18/src/com/granovskiy/CodeWars.java
@@ -0,0 +1,15 @@
+package com.granovskiy;
+
+public class CodeWars {
+    public static int strCount(String str, char letter) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == letter) {
+                count++;
+            }
+        }
+        return count;
+        //  return (int)str.chars().filter(x -> x == letter).count();
+        //  return StringUtils.countMatches(str, letter);
+    }
+}
