41fb63f3eb50d89e3ceea39cec46df994e74632d
Sviatoslav <s.granovskiy@gmail.com>
2019-10-04 11:32:52 +0300

    Initial commit

--- P094-K08-CalculateBMI/src/com/granovskiy/Kata.java
+++ P094-K08-CalculateBMI/src/com/granovskiy/Kata.java
@@ -6,5 +6,11 @@ public class Kata {
         if (weight / (height * height) <= 25.0) return "Normal";
         if (weight / (height * height) <= 30.0) return "Overweight";
         return "Obese";
+
+/*        double bmi = weight / (height * height);
+        if ( bmi <= 18.5) return "Underweight";
+        if ( bmi <= 25) return "Normal";
+        if ( bmi <= 30) return "Overweight";
+        return "Obese";*/
     }
 }
