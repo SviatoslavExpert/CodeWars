7fb684e806b3f6c90dbeab9ab3cf0c990d190728
Sviatoslav <s.granovskiy@gmail.com>
2019-10-05 20:15:30 +0300

    Initial commit

--- P105-K08-TipCalculator/src/com/granovskiy/Kata.java
+++ P105-K08-TipCalculator/src/com/granovskiy/Kata.java
@@ -9,6 +9,14 @@ public class Kata {
         if (s.equals("great")) return (int) Math.ceil(amount * 0.15);
         if (s.equals("excellent"))  return (int) Math.ceil(amount * 0.2);
         return null;
+/*
+        if("Terrible".equalsIgnoreCase(rating))   return 0;
+        if("Poor".equalsIgnoreCase(rating))       return (int)Math.ceil(0.05*amount);
+        if("Good".equalsIgnoreCase(rating))       return (int)Math.ceil(0.10*amount);
+        if("Great".equalsIgnoreCase(rating))      return (int)Math.ceil(0.15*amount);
+        if("Excellent".equalsIgnoreCase(rating))  return (int)Math.ceil(0.20*amount);
+        return null;
+*/
     }
 
     public static void main(String[] args) {
