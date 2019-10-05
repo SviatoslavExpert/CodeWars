8c044cdcc68fd1f44ffb86440189ecd0e935aa7a
Sviatoslav <s.granovskiy@gmail.com>
2019-10-05 15:16:54 +0300

    Initial commit

--- P102-K08-TripleTrouble/src/com/granovskiy/Kata.java
+++ P102-K08-TripleTrouble/src/com/granovskiy/Kata.java
@@ -13,6 +13,13 @@ public class Kata {
         }
         return sb.toString();
     }
+/*
+    StringBuilder sb = new StringBuilder(s1.length()*3);
+    for (int i=0; i<s1.length(); i++) {
+        sb.append(s1.charAt(i)).append(s2.charAt(i)).append(s3.charAt(i));
+    }
+    return sb.toString();
+*/
 
     public static void main(String[] args) {
         System.out.println(tripleTrouble("love", "swim", "here"));
