bda6836bf88f87a701d8e1e1d89030e010cf34a6
Sviatoslav <s.granovskiy@gmail.com>
2019-10-05 11:05:58 +0300

    Initial commit

--- P099-K08-RockPaperScissors/src/com/granovskiy/Kata.java
+++ P099-K08-RockPaperScissors/src/com/granovskiy/Kata.java
@@ -8,11 +8,14 @@ public class Kata {
         if (p1 == "scissors" && p2 == "rock") return "Player 2 won!";
         if (p1 == "paper" && p2 == "rock") return "Player 1 won!";
         if (p1 == "rock" && p2 == "paper") return "Player 2 won!";
-        if (p1 == "rock" && p2 == "rock") return "Draw!";
-        if (p1 == "paper" && p2 == "paper") return "Draw!";
-        if (p1 == "scissors" && p2 == "scissors") return "Draw!";
-        return "";
+        if (p1 == p2) return "Draw!";
+        return "Incorrect input";
     }
+/*
+    if(p1 == p2) return "Draw!";
+    int p = (p1 + p2).equals("scissorspaper") || (p1 + p2).equals("rockscissors") || (p1 + p2).equals("paperrock") ? 1 : 2;
+    return "Player " + p + " won!";
+*/
 
     public static void main(String[] args) {
         System.out.println(rps("rock", "scissors"));
