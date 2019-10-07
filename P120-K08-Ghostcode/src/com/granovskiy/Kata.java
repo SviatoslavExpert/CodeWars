59f5a4d63a8bf9034c632cfbb3724cf087152825
Sviatoslav <s.granovskiy@gmail.com>
2019-10-07 17:37:30 +0300

    Initial commit

---
+++ P120-K08-Ghostcode/src/com/granovskiy/Kata.java
@@ -0,0 +1,11 @@
+package com.granovskiy;
+
+public class Kata {
+    public static String helloName(final String name) {
+        if (name == null || name.equals("")) {
+            return "Hello world!";
+        } else {
+            return "Hello my name is " + name;
+        }
+    }
+}
