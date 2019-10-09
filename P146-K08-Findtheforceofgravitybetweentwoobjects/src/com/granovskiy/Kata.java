176b62071deca612a06e6e7d218e1d4cd0853a87
Sviatoslav <s.granovskiy@gmail.com>
2019-10-09 16:36:42 +0300

    Initial commit

--- P146-K08-Findtheforceofgravitybetweentwoobjects/src/com/granovskiy/Kata.java
+++
@@ -1,5 +0,0 @@
-package com.granovskiy;
-
-public class Kata {
-
-}
---
+++ P146-K08-Mergingsortedintegerarrays/src/com/granovskiy/Kata.java
@@ -0,0 +1,9 @@
+package com.granovskiy;
+
+import java.util.stream.IntStream;
+
+public class Kata {
+    public static int[] mergeArrays(int[] first, int[] second) {
+        return IntStream.concat(IntStream.of(first), IntStream.of(second)).distinct().sorted().toArray();
+    }
+}
