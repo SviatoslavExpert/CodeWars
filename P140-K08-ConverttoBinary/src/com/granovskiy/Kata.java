fac1b0d3bcdedc090fcc09b9f5fcf01a14e7f9a5
Sviatoslav <s.granovskiy@gmail.com>
2019-10-09 14:14:27 +0300

    Initial commit

---
+++ P140-K08-ConverttoBinary/src/com/granovskiy/Kata.java
@@ -0,0 +1,7 @@
+package com.granovskiy;
+
+public class Kata {
+    public static int toBinary(int n) {
+        return Integer.valueOf(Integer.toBinaryString(n));
+    }
+}
---
+++ P141-K08-StringyStrings/src/com/granovskiy/Kata.java
@@ -0,0 +1,20 @@
+package com.granovskiy;
+
+public class Kata {
+    public static String stringy(int size) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            if (i % 2 == 0) {
+                sb.append("1");
+            }
+            if (i % 2 != 0) {
+                sb.append("0");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stringy(18));
+    }
+}
