2c1f95ba8a5b1ee3fdc8a852c8e4a7ddee2e9b15
Sviatoslav <s.granovskiy@gmail.com>
2019-10-09 11:30:38 +0300

    Initial commit

--- P133-K08-Grasshopper-Messigoalsfunction/src/com/granovskiy/Kata.java
+++ P133-K08-Grasshopper-Messigoalsfunction/src/com/granovskiy/Kata.java
@@ -1,11 +1,7 @@
 package com.granovskiy;
 
 public class Kata {
-    public static int summation(int n) {
-        int result = 0;
-        for (int i = 1; i <= n; i++) {
-            result += i;
-        }
-        return result;
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
     }
 }
--- P134-K08-Grasshopper-Summation/src/com/granovskiy/Kata.java
+++ P134-K08-Grasshopper-Summation/src/com/granovskiy/Kata.java
@@ -1,7 +1,13 @@
 package com.granovskiy;
 
 public class Kata {
-    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
-        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    public static int summation(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        return result;
+        //  return  n*(n+1)/2;
+        //  return IntStream.rangeClosed(1,n).sum();
     }
 }
