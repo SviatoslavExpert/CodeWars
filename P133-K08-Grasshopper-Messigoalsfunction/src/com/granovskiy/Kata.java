12d7004c8ce5d41e76bb9ff19437bc8c02463ec7
Sviatoslav <s.granovskiy@gmail.com>
2019-10-09 11:00:16 +0300

    Initial commit

---
+++ P133-K08-Grasshopper-Messigoalsfunction/src/com/granovskiy/Kata.java
@@ -0,0 +1,7 @@
+package com.granovskiy;
+
+public class Kata {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
