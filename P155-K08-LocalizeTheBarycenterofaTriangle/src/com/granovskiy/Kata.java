9d7a5dac86dca121acfb61b6f2134a99d3aa7dfc
Sviatoslav <s.granovskiy@gmail.com>
2019-10-10 14:22:25 +0300

    Initial commit

---
+++ P155-K08-LocalizeTheBarycenterofaTriangle/src/com/granovskiy/Kata.java
@@ -0,0 +1,11 @@
+package com.granovskiy;
+
+public class Kata {
+    public static double[] barTriang(double[] x, double[] y, double[] z)
+    {
+        double[] res = new double[2];
+        res[0] = (double)((int)Math.round((x[0]+y[0]+z[0])/3*10000))/10000;
+        res[1] = (double)((int)Math.round((x[1]+y[1]+z[1])/3*10000))/10000;
+        return res;
+    }
+}
