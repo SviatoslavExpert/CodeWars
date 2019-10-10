47c98e747ac0249489328d235c64787c31f0409a
Sviatoslav <s.granovskiy@gmail.com>
2019-10-10 11:41:28 +0300

    Initial commit

--- P084-K08-FIXMEStaticelectrickery/src/com/granovskiy/Dinglemouse.java
+++ P084-K08-FIXMEStaticelectrickery/src/com/granovskiy/Dinglemouse.java
@@ -2,7 +2,7 @@ package com.granovskiy;
 
 public class Dinglemouse {
 
-    private static int ONE_HUNDRED = 100; // Declare this first!
+    private static int ONE_HUNDRED = 100; // This goes first
 
     public static final Dinglemouse INST = new Dinglemouse();
 
