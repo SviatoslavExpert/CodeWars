d9af1b8913892cb302aedabbbaf8255bf863d1ab
Sviatoslav <s.granovskiy@gmail.com>
2019-10-10 13:44:06 +0300

    Initial commit

--- P091-K08-SortMyTextbooks/src/com/granovskiy/Kata.java
+++ P091-K08-SortMyTextbooks/src/com/granovskiy/Kata.java
@@ -1,14 +1,25 @@
 package com.granovskiy;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Kata {
-    public static List<String> sort(List<String> textbooks) {
-        Collections.sort(textbooks, String.CASE_INSENSITIVE_ORDER);
+/*    public static List<String> sort(List<String> textbooks) {
+        textbooks.replaceAll(String::toLowerCase);
+        Collections.sort(textbooks);
         return textbooks;
-        //     return textbooks.stream()
-        //                    .sorted(String.CASE_INSENSITIVE_ORDER)
-        //                    .collect(toList());
+    }
+*/
+
+    public static List<String> sort(List<String> textbooks) {
+        //textbooks.replaceAll(String::toLowerCase);
+        for (String word : textbooks) {
+            word = word.toLowerCase();
+        }
+        Collections.sort(textbooks);
+        return textbooks;
     }
 }
